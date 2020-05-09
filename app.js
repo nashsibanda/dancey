d6f50bf9d721b50a4efc16e08371ee9dfb669178
nashsibanda <contact@nashsibanda.com>
2020-05-09 15:24:58 +0900

    Add passport authorization, with bearer tokens

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const port = process.env.PORT || 5000;
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const passport = require("passport");
 
 // App Setup
 const app = express();
@@ -16,6 +17,8 @@ mongoose
 // App Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(passport.initialize());
+require("./config/passport")(passport);
 
 // Routes
 app.get("/", (req, res) => res.send("Hello World"));
