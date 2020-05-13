b3a0bbf7c7cb8580abfadb3613040f004f4f33de
Nash Sibanda <contact@nashsibanda.com>
2020-05-13 20:13:48 +0900

    Add personnel validation, id personnel routes

--- routes/api/personnel.js
+++ routes/api/personnel.js
@@ -2,14 +2,47 @@ const express = require("express");
 const router = express.Router();
 
 const Personnel = require("../../models/Personnel");
+const Release = require("../../models/Release");
+const {
+  validateNewPersonnelInput,
+} = require("../../validation/personnelInputValidation");
 
+// GET all personnel
 router.get("/", (req, res) => {
   Personnel.find()
     .sort({ createdAt: -1 })
-    .then(personnel => res.json(personnel))
+    .then(personnelCollection => res.json(personnelCollection))
     .catch(err =>
-      res.status(404).json({ noPersonnelFound: "No personnel found" })
+      res.status(404).json({ noPersonnelCollectionFound: "No personnel found" })
     );
+});
+
+// GET a single release
+router.get("/:id", (req, res) => {
+  Personnel.findById(req.params.id)
+    .then(personnelRecord => res.json(personnelRecord))
+    .catch(err =>
+      res.status(404).json({ noPersonnelRecordFound: "No personnel found" })
+    );
+});
+
+// GET all personnel by release
+router.get("/releases/:release_id", (req, res) => {
+  Release.findById(req.params.release_id)
+    .then(release => {
+      const personnelIds = [];
+      release.personnel.forEach(personnel => {
+        personnelIds.push(personnel.personnelId);
+      });
+      Personnel.find({ _id: { $in: personnelIds } })
+        .then(personnelCollection => res.json(personnelCollection))
+        .catch(err =>
+          res
+            .status(404)
+            .json({ noPersonnelCollectionFound: "No personnel found" })
+        );
+    })
+    .catch(err => res.status(404).json({ noReleaseFound: "No release found" }));
 });
 
 module.exports = router;
