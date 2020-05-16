db4e0fedcb8844b94a315c96f469c3b58010e086
Nash Sibanda <contact@nashsibanda.com>
2020-05-16 15:50:54 +0900

    Add update personnel route and validation

--- routes/api/personnel.js
+++ routes/api/personnel.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const passport = require("passport");
 
 const Personnel = require("../../models/Personnel");
 const Release = require("../../models/Release");
 const {
   validateNewPersonnelInput,
+  validateUpdatePersonnelInput,
 } = require("../../validation/personnelInputValidation");
 
 // GET all personnel
@@ -44,5 +46,53 @@ router.get("/releases/:release_id", (req, res) => {
     })
     .catch(err => res.status(404).json({ noReleaseFound: "No release found" }));
 });
+
+// POST a new personnel record
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validateNewPersonnelInput(req.body);
+    if (!isValid) return res.status(400).json(errors);
+
+    const newPersonnel = new Personnel({
+      ...req.body,
+    });
+
+    newPersonnel
+      .save()
+      .then(newPersonnelRecord => res.json(newPersonnelRecord));
+  }
+);
+
+// PATCH a personnel record
+router.patch(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Personnel.findById(req.params.id)
+      .then(personnelRecord => {
+        const { errors, isValid } = validateUpdatePersonnelInput(req.body);
+        if (!isValid) return res.status(400).json(errors);
+        Personnel.findOneAndUpdate(
+          { _id: personnelRecord._id },
+          {
+            $set: {
+              ...req.body,
+              modifiedAt: Date.now(),
+            },
+          },
+          { new: true },
+          (err, updatedPersonnelRecord) => {
+            if (err) return res.status(400).json(err);
+            return res.json(updatedPersonnelRecord);
+          }
+        );
+      })
+      .catch(err =>
+        res.status(404).json({ noPersonnelRecordFound: "No personnel found" })
+      );
+  }
+);
 
 module.exports = router;
