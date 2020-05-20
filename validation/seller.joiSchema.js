99a7684d8a3f847e661252b5296a303f09b57ebd
Nash Sibanda <contact@nashsibanda.com>
2020-05-21 01:47:20 +0900

    Add seller model and validation schema

---
+++ validation/seller.joiSchema.js
@@ -0,0 +1,22 @@
+const Joi = require("@hapi/joi");
+Joi.objectId = require("joi-objectid")(Joi);
+
+const sellerValidation = Joi.object({
+  sellerName: Joi.string()
+    .max(200)
+    .label("Seller name")
+    .alter({ new: schema => schema.required() }),
+  userId: Joi.objectId()
+    .label("User ID")
+    .alter({ new: schema => schema.required() }),
+  location: Joi.string()
+    .label("Location")
+    .valid(...Object.keys(countries))
+    .messages({
+      "any.only": "Location must be a valid country",
+    }),
+});
+
+const newSellerValidation = sellerValidation.tailor("new");
+
+module.exports = { newSellerValidation, sellerValidation };
