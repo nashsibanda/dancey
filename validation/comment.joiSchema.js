b8afcf3ee5a3d8f4e2684ec656470fa546926dfe
Nash Sibanda <contact@nashsibanda.com>
2020-05-21 23:44:54 +0900

    Restructure comments for polymorphism

--- validation/comment.joiSchema.js
+++ validation/comment.joiSchema.js
@@ -1,6 +1,16 @@
 const Joi = require("@hapi/joi");
 Joi.objectId = require("joi-objectid")(Joi);
 
+const commentTypes = [
+  "release",
+  "personnel",
+  "product",
+  "seller",
+  "track",
+  "review",
+  "buyer",
+];
+
 const commentValidation = Joi.object({
   userId: Joi.objectId()
     .alter({ explicit: schema => schema.required() })
@@ -11,6 +21,13 @@ const commentValidation = Joi.object({
     .label("Comment author username"),
   parentCommentId: Joi.objectId().allow(null).label("Parent comment ID"),
   deleted: Joi.bool().label("Deleted toggle"),
+  resourceType: Joi.string()
+    .label("Resource type")
+    .valid(...commentTypes)
+    .alter({ explicit: schema => schema.required() }),
+  resourceId: Joi.objectId()
+    .label("Resource ID")
+    .alter({ explicit: schema => schema.required() }),
 });
 
 const explicitCommentValidation = commentValidation.tailor("explicit");
