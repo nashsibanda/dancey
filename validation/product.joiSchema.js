9f11b681d188bd187d8b089c2ae02485102035c7
nashsibanda <contact@nashsibanda.com>
2020-05-22 01:50:40 +0900

    Add Product model and validation schema

---
+++ validation/product.joiSchema.js
@@ -0,0 +1,36 @@
+const Joi = require("@hapi/joi");
+Joi.objectId = require("joi-objectid")(Joi);
+
+const currencies = require("./currencies");
+const mediaCondition = require("./mediaCondition");
+
+const productValidation = Joi.object({
+  release: Joi.objectId()
+    .label("Release ID")
+    .alter({ new: schema => schema.required() }),
+  price: Joi.number()
+    .positive()
+    .label("Price")
+    .precision(2)
+    .alter({ new: schema => schema.required() }),
+  currency: Joi.string()
+    .label("Currency")
+    .valid(...Object.keys(currencies))
+    .messages({
+      "any.only": "Currency must be a valid currency",
+    })
+    .alter({ new: schema.required() }),
+  condition: Joi.string()
+    .label("Condition")
+    .valid(...mediaCondition)
+    .messages({
+      "any.only": "Condition must be a valid media condition",
+    })
+    .alter({ new: schema.required() }),
+  sellerId: Joi.objectId().label("Seller ID").alter({ new: schema.required() }),
+  description: Joi.string().label("Description").max(500),
+});
+
+const newProductValidation = productValidation.tailor("new");
+
+module.exports = { productValidation, newProductValidation };
