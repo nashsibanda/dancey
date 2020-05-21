2e53d5f766bb07c46ed4c426f7e9539dd4a57951
Nash Sibanda <contact@nashsibanda.com>
2020-05-22 01:50:40 +0900

    Add Product model and validation schema

---
+++ models/Product.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const currencies = require("../validation/currencies");
+const mediaCondition = require("../validation/mediaCondition");
+
+const ProductSchema = new Schema(
+  {
+    releaseId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    currency: {
+      type: String,
+      enum: Object.keys(currencies),
+      required: true,
+    },
+    condition: {
+      type: String,
+      enum: Object.keys(mediaCondition),
+      required: true,
+    },
+    sellerId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+    },
+    description: String,
+  },
+  { timestamps: true }
+);
+
+module.exports = Product = mongoose.model("Product", ProductSchema);
