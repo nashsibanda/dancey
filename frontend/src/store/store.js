4211e1b14efb1ded6c5a9d407ad0764a9d19383f
Nash Sibanda <contact@nashsibanda.com>
2020-05-25 21:17:11 +0900

    Add devtools to store

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,9 +1,14 @@
 import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import logger from "redux-logger";
 import rootReducer from "../reducers/root_reducer";
 
 const configureStore = (preloadedState = {}) =>
-  createStore(rootReducer, preloadedState, applyMiddleware(thunk, logger));
+  createStore(
+    rootReducer,
+    preloadedState,
+    composeWithDevTools(applyMiddleware(thunk, logger))
+  );
 
 export default configureStore;
