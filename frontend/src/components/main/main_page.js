1431fa324b2592079a283706c8d522ad8cfe8464
nashsibanda <contact@nashsibanda.com>
2020-05-25 14:42:27 +0900

    Basic app skeleton, with navbar

---
+++ frontend/src/components/main/main_page.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class MainPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Dancey!</h1>
+        <footer>Copyright &copy; 2020 Dancey</footer>
+      </div>
+    );
+  }
+}
+
+export default MainPage;
