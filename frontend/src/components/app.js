ec7466900d1807552136ed4c321eea5d1e0e091e
nashsibanda <contact@nashsibanda.com>
2020-05-25 22:10:55 +0900

    Add releases API and initial actions, reducer

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import { AuthRoute, ProtectedRoute } from "../util/route_util";
 import MainPage from "./main/main_page";
 import LoginFormContainer from "./session/login_form_container";
@@ -10,9 +10,9 @@ const App = () => (
   <div>
     <NavBarContainer />
     <Switch>
-      <ProtectedRoute exact path="/" component={MainPage} />
       <AuthRoute exact path="/login" component={LoginFormContainer} />
       <AuthRoute exact path="/register" component={RegisterFormContainer} />
+      <Route exact path="/" component={MainPage} />
     </Switch>
   </div>
 );
--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import ErrorsReducer from "./errors_reducer";
 import SessionReducer from "./session_reducer";
+import ReleasesReducer from "./releases_reducer";
 
 const rootReducer = combineReducers({
   errors: ErrorsReducer,
   session: SessionReducer,
+  releases: ReleasesReducer,
 });
 
 export default rootReducer;
