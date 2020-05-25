b00bb3162513b7b0085af40c5cf19df2a7e78678
nashsibanda <contact@nashsibanda.com>
2020-05-25 14:41:56 +0900

    Add session routes to API, preloaded auth

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,7 @@ import jwt_decode from "jwt-decode";
 import { setAuthToken } from "./util/session_api_util";
 import configureStore from "./store/store";
 import Root from "./components/root";
+import { logout } from "./actions/session_actions";
 
 document.addEventListener("DOMContentLoaded", () => {
   let store;
--- frontend/src/util/session_api_util.js
+++ frontend/src/util/session_api_util.js
@@ -7,3 +7,11 @@ export const setAuthToken = token => {
     delete axios.defaults.headers.common["Authorization"];
   }
 };
+
+export const postNewUser = newUserData => {
+  return axios.post("/api/users/register", newUserData);
+};
+
+export const postNewSession = loginUserData => {
+  return axios.post("/api/users/login", loginUserData);
+};
