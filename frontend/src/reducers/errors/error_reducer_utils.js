44cf2b22549d81b12389363bddc9c5785535a016
nashsibanda <contact@nashsibanda.com>
2020-05-27 16:13:00 +0900

    Parse errors better for react

--- frontend/src/reducers/errors/error_reducer_utils.js
+++ frontend/src/reducers/errors/error_reducer_utils.js
@@ -1,15 +1,9 @@
 export const parseErrors = errors => {
   let outputErrors = [];
-  const errorMessageArray = errors.message
-    .split(".")
-    .map(message => message.trim());
-
   errors.message.split(".").forEach(message => {
     let error = { name: errors.name, message: message.trim() };
     outputErrors.push(error);
   });
 
   return outputErrors;
-
-  // return { name: errors.name, messages: errorMessageArray };
 };
