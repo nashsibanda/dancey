7455b6834b664f594e0eb786404f60d26817a6e7
nashsibanda <contact@nashsibanda.com>
2020-05-27 16:16:06 +0900

    Add styles skeleton in sass

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,7 @@ import { setAuthToken } from "./util/session_api_util";
 import configureStore from "./store/store";
 import Root from "./components/root";
 import { logout } from "./actions/session_actions";
+import "./styles/main.scss";
 
 document.addEventListener("DOMContentLoaded", () => {
   let store;
