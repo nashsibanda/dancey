b21a05d1c08981f920ca866389095f297133f64d
Nash Sibanda <contact@nashsibanda.com>
2020-05-29 02:39:42 +0900

    Split incoming release data into entity reducers

--- frontend/src/reducers/entities_reducer.js
+++ frontend/src/reducers/entities_reducer.js
@@ -1,8 +1,16 @@
 import { combineReducers } from "redux";
 import ReleasesReducer from "./entities/releases_reducer";
+import PersonnelReducer from "./entities/personnel_reducer";
+import TracksReducer from "./entities/tracks_reducer";
+import CommentsReducer from "./entities/comments_reducer";
+import ReviewsReducer from "./entities/reviews_reducer";
 
 const EntitiesReducer = combineReducers({
   releases: ReleasesReducer,
+  personnel: PersonnelReducer,
+  tracks: TracksReducer,
+  comments: CommentsReducer,
+  reviews: ReviewsReducer,
 });
 
 export default EntitiesReducer;
