8ccfcfe8b3530150abf87dcf1e072022c76b51a8
nashsibanda <contact@nashsibanda.com>
2020-05-29 01:13:14 +0900

    Restructure entities into combined reducer

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -1,12 +1,12 @@
 import { combineReducers } from "redux";
 import ErrorsReducer from "./errors_reducer";
 import SessionReducer from "./session_reducer";
-import ReleasesReducer from "./releases_reducer";
+import EntitiesReducer from "./entities_reducer";
 
 const rootReducer = combineReducers({
   errors: ErrorsReducer,
   session: SessionReducer,
-  releases: ReleasesReducer,
+  entities: EntitiesReducer,
 });
 
 export default rootReducer;
