a455e259178bbef795462d69b4742a5f879f659c
Nash Sibanda <contact@nashsibanda.com>
2020-05-29 01:12:11 +0900

    Refactor link formatter for all resources

--- frontend/src/util/formatting_util.js
+++ frontend/src/util/formatting_util.js
@@ -1,32 +1,29 @@
 import React from "react";
-import parse from "html-react-parser";
 import { Link } from "react-router-dom";
 
-export const joinMainArtistLinksBad = mainArtists => {
-  const linksArray = mainArtists.map(artistObj => {
-    return <Link to={`api/personnel/${artistObj._id}`}>{artistObj.name}</Link>;
-  });
-  console.log(`linksArray = ${JSON.stringify(linksArray)}`);
-  return parse(linksArray.join());
-};
-
-export const joinMainArtistLinks = mainArtists => {
-  if (mainArtists.length > 0) {
+export const joinObjectLinks = (
+  objectArray,
+  collection = "personnel",
+  identifier = "name"
+) => {
+  if (objectArray.length > 0) {
     return (
       <>
-        {mainArtists.slice(0, -1).map(({ _id, name }) => (
+        {objectArray.slice(0, -1).map(object => (
           <>
-            <Link to={`/api/personnel/${_id}`}>{name}</Link>
+            <Link to={`/${collection}/${object._id}`}>
+              {object[identifier]}
+            </Link>
             {", "}
           </>
         ))}
-        <Link to={`/api/personnel/${mainArtists[mainArtists.length - 1]._id}`}>
-          {mainArtists[mainArtists.length - 1].name}
+        <Link to={`/${collection}/${objectArray[objectArray.length - 1]._id}`}>
+          {objectArray[objectArray.length - 1][identifier]}
         </Link>
       </>
     );
   } else {
-    return <span>No Artist</span>;
+    return <></>;
   }
 };
 
