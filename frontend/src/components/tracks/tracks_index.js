20927b7eff72d833d4a0083f0ec0ca180246c306
nashsibanda <contact@nashsibanda.com>
2020-05-31 20:49:23 +0900

    Add tracks index styles

--- frontend/src/components/tracks/tracks_index.js
+++ frontend/src/components/tracks/tracks_index.js
@@ -1,9 +1,14 @@
 import React, { Component } from "react";
 import TracksIndexItem from "./tracks_index_item";
+import { makeFriendlyTime } from "../../util/formatting_util";
 
 export default class TracksIndex extends Component {
   render() {
     const { stateTracks, releaseTracks } = this.props;
+    const totalDuration = releaseTracks.reduce(
+      (acc, track) => acc + track.trackId.duration,
+      0
+    );
     return (
       <ul className="tracks-index">
         {releaseTracks.length > 0 &&
@@ -21,6 +26,13 @@ export default class TracksIndex extends Component {
                 )
               );
             })}
+        <li className="tracks-index-item">
+          <div className="main-track-details">
+            <span className="tracks-total-duration">
+              Total Duration: {makeFriendlyTime(totalDuration)}
+            </span>
+          </div>
+        </li>
       </ul>
     );
   }
