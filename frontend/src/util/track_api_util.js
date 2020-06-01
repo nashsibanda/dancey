11d171bea879d1b246c1c911548b36711650e88d
Nash Sibanda <contact@nashsibanda.com>
2020-06-01 15:27:38 +0900

    Rename getTrack, receiveTrack methods

--- frontend/src/util/track_api_util.js
+++ frontend/src/util/track_api_util.js
@@ -2,16 +2,10 @@ import axios from "axios";
 
 export const getAllTracks = () => axios.get("/api/tracks/");
 
-export const getOneTrack = id => axios.get(`/api/tracks/${id}`);
+export const getTrack = id => axios.get(`/api/tracks/${id}`);
 
 export const getResourceTracks = (resourceType, resourceId) =>
   axios.get(`/api/tracks/get/${resourceType}/${resourceId}`);
-
-// export const getPersonnelTracks = personnelId =>
-//   axios.get(`/api/tracks/get/personnel/${personnelId}`);
-
-// export const getReleaseTracks = releaseId =>
-//   axios.get(`/api/tracks/get/release/${releaseId}`);
 
 export const postTrack = trackPostData =>
   axios.post("/api/tracks/", trackPostData);
