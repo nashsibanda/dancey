e27f9dcc98ea4263641c64b2ee81457ef0547957
Nash Sibanda <contact@nashsibanda.com>
2020-06-01 15:28:04 +0900

    Add tracks and releases errors to reducer

--- frontend/src/reducers/errors_reducer.js
+++ frontend/src/reducers/errors_reducer.js
@@ -1,8 +1,12 @@
 import { combineReducers } from "redux";
 import SessionErrorsReducer from "./errors/session_errors_reducer";
+import ReleaseErrorsReducer from "./errors/releases_errors_reducer";
+import TracksErrorsReducer from "./errors/tracks_errors_reducer";
 
 const ErrorsReducer = combineReducers({
   session: SessionErrorsReducer,
+  releases: ReleaseErrorsReducer,
+  tracks: TracksErrorsReducer,
 });
 
 export default ErrorsReducer;
