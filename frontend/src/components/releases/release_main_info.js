f21bd5a6236a8757f4e9407ac6b310ef42b1f024
nashsibanda <contact@nashsibanda.com>
2020-06-01 09:53:17 +0900

    Add put frontend method for updating fields

--- frontend/src/components/releases/release_main_info.js
+++ frontend/src/components/releases/release_main_info.js
@@ -1,51 +1,114 @@
 import React from "react";
 import { joinObjectLinks } from "../../util/formatting_util";
 
-export default function ReleaseMainInfo(props) {
-  const {
-    images,
-    mainArtists,
-    label,
-    format,
-    releaseCountry,
-    releaseYear,
-    title,
-  } = props.release;
-  const mainImage = images.find(({ mainImage }) => mainImage === true);
-  return (
-    <div className="resource-main-info">
-      <div className="resource-image">
-        <img
-          src={mainImage ? mainImage.imageUrl : "/assets/plain_record.png"}
-          alt={
-            mainImage
-              ? mainImage.description
-              : "Default album placeholder image - upload a new one!"
-          }
-        />
-        <button className="big-button">More Images</button>
+export default class ReleaseMainInfo extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      releaseYear: this.props.release.releaseYear,
+      editYear: true,
+    };
+
+    this.toggleForm = this.toggleForm.bind(this);
+    this.updateField = this.updateField.bind(this);
+  }
+
+  toggleForm(form) {
+    return e => this.setState({ [form]: !this.state[form] });
+  }
+
+  updateField(field) {
+    if (field === "releaseYear") {
+      return e => this.setState({ [field]: parseInt(e.currentTarget.value) });
+    } else {
+      return e => this.setState({ [field]: e.currentTarget.value });
+    }
+  }
+
+  handleSubmit(field) {
+    return e => {
+      e.preventDefault();
+      const updateData = {
+        [field]: this.state[field],
+      };
+      this.props.updateRelease(this.props.release._id, updateData);
+    };
+  }
+
+  render() {
+    const { loggedIn, release } = this.props;
+    const {
+      images,
+      mainArtists,
+      label,
+      format,
+      releaseCountry,
+      // releaseYear,
+      title,
+    } = release;
+    const { editYear, releaseYear } = this.state;
+
+    const mainImage = images.find(({ mainImage }) => mainImage === true);
+
+    return (
+      <div className="resource-main-info">
+        <div className="resource-image">
+          <img
+            src={mainImage ? mainImage.imageUrl : "/assets/plain_record.png"}
+            alt={
+              mainImage
+                ? mainImage.description
+                : "Default album placeholder image - upload a new one!"
+            }
+          />
+          <button className="big-button">More Images</button>
+        </div>
+        <div className="resource-details">
+          <h2>
+            {joinObjectLinks(mainArtists)} — {title}
+          </h2>
+          <div>
+            <span className="details-label">Label:</span>
+            <span className="details-value">{joinObjectLinks(label)}</span>
+          </div>
+          <div>
+            <span className="details-label">Format:</span>
+            <span className="details-value">{format}</span>
+          </div>
+          <div>
+            <span className="details-label">Release Country:</span>
+            <span className="details-value">{releaseCountry}</span>
+          </div>
+          <div>
+            <span className="details-label">Release Year:</span>
+            {editYear ? (
+              <form onSubmit={this.handleSubmit("releaseYear")}>
+                <input
+                  type="number"
+                  min="1890"
+                  max="2030"
+                  value={releaseYear}
+                  onChange={this.updateField("releaseYear")}
+                ></input>
+                <button type="submit">Submit</button>
+                <button type="button" onClick={this.toggleForm("editYear")}>
+                  Cancel
+                </button>
+              </form>
+            ) : (
+              <span className="details-value">
+                {releaseYear}
+                {loggedIn && (
+                  <button type="button" onClick={this.toggleForm("editYear")}>
+                    Edit
+                  </button>
+                )}
+              </span>
+            )}
+          </div>
+        </div>
       </div>
-      <div className="resource-details">
-        <h2>
-          {joinObjectLinks(mainArtists)} — {title}
-        </h2>
-        <div>
-          <span className="details-label">Label:</span>
-          <span className="details-value">{joinObjectLinks(label)}</span>
-        </div>
-        <div>
-          <span className="details-label">Format:</span>
-          <span className="details-value">{format}</span>
-        </div>
-        <div>
-          <span className="details-label">Release Country:</span>
-          <span className="details-value">{releaseCountry}</span>
-        </div>
-        <div>
-          <span className="details-label">Release Year:</span>
-          <span className="details-value">{releaseYear}</span>
-        </div>
-      </div>
-    </div>
-  );
+    );
+  }
 }
