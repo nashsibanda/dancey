6e7c74454be54aae10f2aafff04829760f17ab3d
nashsibanda <contact@nashsibanda.com>
2020-06-02 01:15:01 +0900

    Catch string errors for errors reducer parser

--- frontend/src/reducers/errors/error_reducer_utils.js
+++ frontend/src/reducers/errors/error_reducer_utils.js
@@ -1,4 +1,6 @@
 export const parseErrors = errors => {
+  if (typeof errors === "string")
+    return [{ name: "StandardError", message: errors }];
   let outputErrors = [];
   errors.message.split(".").forEach(message => {
     let error = { name: errors.name, message: message.trim() };
