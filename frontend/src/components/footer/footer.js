44d3ed290657566cf83132bc603b363ec7b8467e
Nash Sibanda <contact@nashsibanda.com>
2020-06-02 02:44:55 +0900

    Add global site footer

---
+++ frontend/src/components/footer/footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Footer(props) {
+  return (
+    <footer className="site-footer">
+      <ul className="footer-menu">
+        <li>
+          <a href="http://nashsibanda.com">nashsibanda.com</a>
+        </li>
+        <li>
+          <a href="https://github.com/nashsibanda/picbait">github</a>
+        </li>
+      </ul>
+    </footer>
+  );
+}
