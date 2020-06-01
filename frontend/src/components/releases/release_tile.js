5e009222a54084648e2c0117ead5ade944e21ce9
nashsibanda <contact@nashsibanda.com>
2020-06-02 03:01:41 +0900

    Tweak styles on release index tiles

--- frontend/src/components/releases/release_tile.js
+++ frontend/src/components/releases/release_tile.js
@@ -17,23 +17,31 @@ const ReleaseTile = props => {
 
   return (
     <li className="release-tile">
-      <img
-        src={mainImage ? mainImage.imageUrl : plainRecordImage}
-        alt={
-          mainImage
-            ? mainImage.description
-            : "Default album placeholder image - upload a new one!"
-        }
-      />
+      <Link to={`/releases/${_id}`}>
+        <img
+          src={mainImage ? mainImage.imageUrl : plainRecordImage}
+          alt={
+            mainImage
+              ? mainImage.description
+              : "Default album placeholder image - upload a new one!"
+          }
+        />
+      </Link>
       <div className="release-tile-info">
-        <div>
+        <div className="release-tile-title">
           <Link to={`/releases/${_id}`}>{title}</Link>
         </div>
-        <div>{mainArtists[0] && joinObjectLinks(mainArtists)}</div>
-        <div>
-          <span>{releaseYear}</span>
-          <span>{format}</span>
-          <span>{releaseCountry}</span>
+        <div className="release-tile-artist">
+          {mainArtists[0] && joinObjectLinks(mainArtists)}
+        </div>
+        <div className="release-tile-details">
+          <div className="release-tile-details-format-year">
+            <span>{releaseYear}</span>
+            <span>{format}</span>
+          </div>
+          <div className="release-tile-details-country">
+            <span>{releaseCountry}</span>
+          </div>
         </div>
       </div>
     </li>
