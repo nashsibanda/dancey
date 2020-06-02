bd581840bc532105208f1bb4ae9027914ced38c0
nashsibanda <contact@nashsibanda.com>
2020-06-02 14:05:14 +0900

    Add react-fontawesome, nodemon config

---
+++ frontend/src/util/fontawesome_library.js
@@ -0,0 +1,34 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faEdit,
+  faToggleOn,
+  faToggleOff,
+  faComment,
+  faPlusSquare,
+  faMinusSquare,
+  faPenFancy,
+  faThumbsUp,
+  faReply,
+  faSave,
+  faSignInAlt,
+  faSignOutAlt,
+  faUndoAlt,
+  faImages,
+} from "@fortawesome/free-solid-svg-icons";
+
+library.add(
+  faEdit,
+  faToggleOff,
+  faToggleOn,
+  faComment,
+  faPlusSquare,
+  faMinusSquare,
+  faPenFancy,
+  faThumbsUp,
+  faReply,
+  faSave,
+  faSignInAlt,
+  faSignOutAlt,
+  faUndoAlt,
+  faImages
+);
