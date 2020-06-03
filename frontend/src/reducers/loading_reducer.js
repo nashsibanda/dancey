ea7b2d36c9aa31613666bafe5550afd324ea8126
nashsibanda <contact@nashsibanda.com>
2020-06-03 16:35:54 +0900

    Add session loading reducer for login/register

--- frontend/src/reducers/loading_reducer.js
+++ frontend/src/reducers/loading_reducer.js
@@ -5,6 +5,7 @@ import CommentsLoadingReducer from "./loading/comments_loading_reducer";
 import TracksLoadingReducer from "./loading/tracks_loading_reducer";
 import PersonnelLoadingReducer from "./loading/personnel_loading_reducer";
 import TrackPersonnelLoadingReducer from "./loading/track_personnel_loading_reducer";
+import SessionLoadingReducer from "./loading/session_loading_reducer";
 
 const LoadingReducer = combineReducers({
   releases: ReleasesLoadingReducer,
@@ -13,6 +14,7 @@ const LoadingReducer = combineReducers({
   tracks: TracksLoadingReducer,
   personnel: PersonnelLoadingReducer,
   trackPersonnel: TrackPersonnelLoadingReducer,
+  session: SessionLoadingReducer,
 });
 
 export default LoadingReducer;
