847727ff2cc0b18d4a9969ec93eedcadb0d057db
Nash Sibanda <contact@nashsibanda.com>
2020-06-03 16:36:25 +0900

    Add session timeout, loading and modal dispatches

--- frontend/src/actions/session_actions.js
+++ frontend/src/actions/session_actions.js
@@ -1,10 +1,15 @@
 import * as SessionAPIUtil from "../util/session_api_util";
 import jwt_decode from "jwt-decode";
+import { sessionLoadingOn, sessionLoadingOff } from "./loading_actions";
 
 export const RECEIVE_USER_LOGOUT = "RECEIVE_USER_LOGOUT";
 export const RECEIVE_SESSION_ERRORS = "RECEIVE_SESSION_ERRORS";
+export const CLEAR_SESSION_ERRORS = "CLEAR_SESSION_ERRORS";
 export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
 export const RECEIVE_USER_LOGIN = "RECEIVE_USER_LOGIN";
+export const SHOW_SESSION_FORM_MODAL = "SHOW_SESSION_FORM_MODAL";
+export const HIDE_SESSION_FORM_MODAL = "HIDE_SESSION_FORM_MODAL";
+export const TOGGLE_SESSION_FORM_MODAL = "TOGGLE_SESSION_FORM_MODAL";
 
 export const receiveCurrentUser = currentUser => ({
   type: RECEIVE_CURRENT_USER,
@@ -20,6 +25,22 @@ export const receiveSessionErrors = errors => ({
   errors,
 });
 
+export const clearSessionErrors = () => ({
+  type: CLEAR_SESSION_ERRORS,
+});
+
+export const showSessionFormModal = () => ({
+  type: SHOW_SESSION_FORM_MODAL,
+});
+
+export const hideSessionFormModal = () => ({
+  type: HIDE_SESSION_FORM_MODAL,
+});
+
+export const toggleSessionFormModal = () => ({
+  type: TOGGLE_SESSION_FORM_MODAL,
+});
+
 export const logoutUser = () => ({
   type: RECEIVE_USER_LOGOUT,
 });
@@ -30,13 +51,28 @@ export const logout = () => dispatch => {
   dispatch(logoutUser());
 };
 
-export const register = user => dispatch =>
+export const register = user => dispatch => {
+  dispatch(sessionLoadingOn());
   SessionAPIUtil.postNewUser(user).then(
-    () => dispatch(receiveUserLogin()),
-    err => dispatch(receiveSessionErrors(err.response.data))
+    res => {
+      const { token } = res.data;
+      localStorage.setItem("jwtToken", token);
+      SessionAPIUtil.setAuthToken(token);
+      const decodedUser = jwt_decode(token);
+      dispatch(receiveCurrentUser(decodedUser));
+      dispatch(hideSessionFormModal());
+      dispatch(sessionLoadingOff());
+      setLogoutTimeout(decodedUser, dispatch);
+    },
+    err => {
+      dispatch(receiveSessionErrors(err.response.data));
+      dispatch(sessionLoadingOff());
+    }
   );
+};
 
-export const login = user => dispatch =>
+export const login = user => dispatch => {
+  dispatch(sessionLoadingOn());
   SessionAPIUtil.postNewSession(user)
     .then(res => {
       const { token } = res.data;
@@ -44,5 +80,24 @@ export const login = user => dispatch =>
       SessionAPIUtil.setAuthToken(token);
       const decodedUser = jwt_decode(token);
       dispatch(receiveCurrentUser(decodedUser));
+      dispatch(hideSessionFormModal());
+      dispatch(sessionLoadingOff());
+      setLogoutTimeout(decodedUser, dispatch);
     })
-    .catch(err => dispatch(receiveSessionErrors(err.response.data)));
+    .catch(err => {
+      dispatch(receiveSessionErrors(err.response.data));
+      dispatch(sessionLoadingOff());
+    });
+};
+
+const setLogoutTimeout = (decodedUser, dispatch) => {
+  setTimeout(() => {
+    dispatch(logout());
+    dispatch(
+      receiveSessionErrors({
+        name: "SessionTimeout",
+        message: "Your session has timed out. Please log in again.",
+      })
+    );
+  }, (decodedUser.exp - decodedUser.iat) * 1000);
+};
