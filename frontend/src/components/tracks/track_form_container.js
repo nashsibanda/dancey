e382ae0659ca71331bf18b4cecb6dc6f5d4a7036
Nash Sibanda <contact@nashsibanda.com>
2020-06-05 02:03:40 +0900

    Add default selected to autocomplete

--- frontend/src/components/tracks/track_form_container.js
+++ frontend/src/components/tracks/track_form_container.js
@@ -2,10 +2,16 @@ import { createTrack, createTrackListing } from "../../actions/track_actions";
 import { connect } from "react-redux";
 import TrackForm from "./track_form";
 
+const mapStateToProps = state => ({
+  stateTracks: state.entities.tracks,
+  statePersonnel: state.entities.personnel,
+  stateReleases: state.entities.releases,
+});
+
 const mapDispatchToProps = dispatch => ({
   createTrack: trackData => dispatch(createTrack(trackData)),
   createTrackListing: (trackData, releaseId) =>
     dispatch(createTrackListing(trackData, releaseId)),
 });
 
-export default connect(null, mapDispatchToProps)(TrackForm);
+export default connect(mapStateToProps, mapDispatchToProps)(TrackForm);
