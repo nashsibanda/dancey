790da5674960581b25f2aba00a3938cfbaec76fe
nashsibanda <contact@nashsibanda.com>
2020-06-05 17:23:30 +0900

    Add analog sides labels, trim formats

--- validation/formats.js
+++ validation/formats.js
@@ -1,13 +1,3 @@
-const formats = [
-  "LP",
-  "Double LP",
-  "Casette",
-  "Double Casette",
-  "CD",
-  "Double CD",
-  "MiniDisc",
-  "Digital",
-  "8-Track",
-];
+const formats = ["LP", "Cassette", "CD", "MiniDisc"];
 
 module.exports = formats;
