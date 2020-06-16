cb2a5343467b703f2afb55cbe6aaddd96030f553
nashsibanda <contact@nashsibanda.com>
2020-06-16 23:12:46 +0900

    Add comment form functionality, with styles

--- frontend/src/components/comments/comment_form_container.js
+++ frontend/src/components/comments/comment_form_container.js
@@ -2,8 +2,12 @@ import { connect } from "react-redux";
 import { createNewComment } from "../../actions/comment_actions";
 import CommentForm from "./comment_form";
 
+const mapStateToProps = state => ({
+  currentUser: state.session.user,
+});
+
 const mapDispatchToProps = dispatch => ({
   createNewComment: commentData => dispatch(createNewComment(commentData)),
 });
 
-export default connect(null, mapDispatchToProps)(CommentForm);
+export default connect(mapStateToProps, mapDispatchToProps)(CommentForm);
