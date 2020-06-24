3dfa1b0333cb7dce69a381bf232ece2bdd7909c5
Nash Sibanda <contact@nashsibanda.com>
2020-06-25 01:49:44 +0900

    Add image action for adding images to resources

---
+++ frontend/src/actions/image_actions.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+import { receiveReleaseErrors, receiveRelease } from "./release_actions";
+
+const resourceSwitch = resourceType => {
+  switch (resourceType) {
+    case "release":
+      return {
+        errors: receiveReleaseErrors,
+        receiveOne: receiveRelease,
+      };
+    default:
+      break;
+  }
+};
+
+export const addResourceImage = (
+  resourceType,
+  resourceId,
+  formData
+) => dispatch => {
+  const config = {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  };
+
+  axios
+    .put(`/api/images/add/${resourceType}/${resourceId}`, formData, config)
+    .then(resource =>
+      dispatch(resourceSwitch(resourceType).receiveOne(resource.data))
+    )
+    .catch(err =>
+      dispatch(resourceSwitch(resourceType).errors(err.response.data))
+    );
+};
