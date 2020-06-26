8133ef7c6eee1af16950b8d53dee4443d8cc62a9
nashsibanda <contact@nashsibanda.com>
2020-06-26 16:04:26 +0900

    Add image modal styles, edit images button

--- frontend/src/components/images/images_modal.js
+++ frontend/src/components/images/images_modal.js
@@ -5,21 +5,38 @@ import ImageFormContainer from "./image_form_container";
 export default class ImagesModal extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      showNewForm: false,
+    };
     this.stopClickPropagation = this.stopClickPropagation.bind(this);
+    this.toggleNewForm = this.toggleNewForm.bind(this);
   }
 
   stopClickPropagation(e) {
     e.stopPropagation();
   }
 
+  toggleNewForm() {
+    this.setState({ showNewForm: !this.state.showNewForm });
+  }
+
   render() {
-    const { toggleImageModal, resourceType, resourceId } = this.props;
+    const { toggleImageModal, resourceType, resourceId, loggedIn } = this.props;
     return (
       <div className="images-modal" onClick={toggleImageModal}>
         <div
           className="images-modal-content-wrapper"
           onClick={this.stopClickPropagation}
         >
+          {loggedIn && (
+            <button
+              className="big-button edit-images-button"
+              onClick={this.toggleNewForm}
+            >
+              <FontAwesomeIcon icon="edit" />
+              <span>Edit Images</span>
+            </button>
+          )}
           <button
             className="big-button modal-close-button"
             onClick={toggleImageModal}
@@ -27,10 +44,15 @@ export default class ImagesModal extends Component {
             <FontAwesomeIcon icon="times" />
           </button>
           <div className="images-modal-content">
-            <ImageFormContainer
-              resourceType={resourceType}
-              resourceId={resourceId}
-            />
+            {this.state.showNewForm ? (
+              <ImageFormContainer
+                resourceType={resourceType}
+                resourceId={resourceId}
+                toggleNewForm={this.toggleNewForm}
+              />
+            ) : (
+              <h2>NOT</h2>
+            )}
           </div>
         </div>
       </div>
