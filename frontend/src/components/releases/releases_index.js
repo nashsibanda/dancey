5e5a25feb079a93ad727d475a6b11492cd666f88
Nash Sibanda <contact@nashsibanda.com>
2020-07-09 01:06:34 +0900

    Add page navigation and pagination to index

--- frontend/src/components/releases/releases_index.js
+++ frontend/src/components/releases/releases_index.js
@@ -8,17 +8,74 @@ class ReleasesIndex extends React.Component {
     super(props);
 
     this.state = {
-      // initialLoad: false;
+      initialLoad: false,
+      itemsPerPage: 50,
+      pageNum: 1,
     };
+
+    this.changePageNumber = this.changePageNumber.bind(this);
+    this.changePageSize = this.changePageSize.bind(this);
   }
 
   componentDidMount() {
-    const { resourceType, resourceId } = this.props;
-    this.props.fetchReleases(resourceType, resourceId);
+    const {
+      resourceType,
+      resourceId,
+      fetchReleases,
+      getReleasesCount,
+    } = this.props;
+    const { pageNum, itemsPerPage } = this.state;
+    fetchReleases(pageNum, itemsPerPage, resourceType, resourceId);
+    getReleasesCount();
+    this.setState({ initialLoad: true });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.pageNum !== this.state.pageNum) {
+      this.props.fetchReleases(
+        this.state.pageNum,
+        this.state.itemsPerPage,
+        this.props.resourceType,
+        this.props.resourceId
+      );
+    }
+    if (prevState.itemsPerPage !== this.state.itemsPerPage) {
+      this.setState({ pageNum: 1 }, () => {
+        this.props.fetchReleases(
+          this.state.pageNum,
+          this.state.itemsPerPage,
+          this.props.resourceType,
+          this.props.resourceId
+        );
+      });
+    }
+  }
+
+  changePageNumber(number) {
+    return e => this.setState({ pageNum: parseInt(number) });
+  }
+
+  changePageSize(e) {
+    this.setState({ itemsPerPage: parseInt(e.target.value) });
   }
 
   render() {
-    const { mainCatalogue, releases, indexTitle, loading } = this.props;
+    const {
+      mainCatalogue,
+      releases,
+      indexTitle,
+      loading,
+      releasesIndexCount,
+    } = this.props;
+    const { initialLoad, itemsPerPage, pageNum } = this.state;
+
+    const maxPageNumber = Math.ceil(releasesIndexCount / itemsPerPage);
+    const pages = [];
+
+    for (let i = 0; i < maxPageNumber; i++) {
+      pages.push(i + 1);
+    }
+
     return (
       <div className="releases-index-container">
         {!mainCatalogue && (
@@ -31,15 +88,108 @@ class ReleasesIndex extends React.Component {
             <title>Releases — dancey</title>
           </Helmet>
         )}
-        {loading ? (
-          <LoadingSpinner />
-        ) : (
-          <ul className="releases-index">
-            {Object.values(releases).map(release => (
-              <ReleaseTile key={release._id} release={release} />
-            ))}
-          </ul>
-        )}
+        <div className="releases-index-view-options">
+          <span className="index-page-size-select">
+            <label>
+              Items Per Page
+              <select onChange={this.changePageSize} defaultValue={50}>
+                <option value={25}>25</option>
+                <option value={50}>50</option>
+                <option value={75}>75</option>
+                <option value={100}>100</option>
+                <option value={150}>150</option>
+                <option value={250}>250</option>
+              </select>
+            </label>
+          </span>
+          <span className="index-page-navigation">
+            <button
+              className="link-button"
+              disabled={pageNum === 1}
+              onClick={this.changePageNumber(1)}
+            >
+              First
+            </button>
+            <button
+              className="link-button"
+              disabled={pageNum === 1}
+              onClick={this.changePageNumber(pageNum - 1)}
+            >
+              Prev
+            </button>
+            <ul className="page-navigation-numbers">
+              <button
+                className={`link-button ${pageNum === 1 ? "selected" : ""}`}
+                onClick={this.changePageNumber(1)}
+              >
+                1
+              </button>
+              {pageNum !== 1 && pageNum - 1 > 2 && <span>...</span>}
+              {pages.map(
+                page =>
+                  (page === pageNum ||
+                    page - pageNum === 1 ||
+                    pageNum - page === 1) &&
+                  page !== 1 &&
+                  page !== maxPageNumber && (
+                    <li key={`page-num-${page}`}>
+                      <button
+                        className={`link-button ${
+                          page === pageNum ? "selected" : ""
+                        }`}
+                        onClick={this.changePageNumber(page)}
+                      >
+                        {page}
+                      </button>
+                    </li>
+                  )
+              )}
+              {pageNum !== maxPageNumber &&
+                pageNum + 1 !== maxPageNumber &&
+                pageNum + 2 !== maxPageNumber && <span>...</span>}
+              <button
+                className={`link-button ${
+                  pageNum === maxPageNumber ? "selected" : ""
+                }`}
+                onClick={this.changePageNumber(maxPageNumber)}
+              >
+                {maxPageNumber}
+              </button>
+            </ul>
+            <button
+              className="link-button"
+              disabled={pageNum === maxPageNumber}
+              onClick={this.changePageNumber(pageNum + 1)}
+            >
+              Next
+            </button>
+            <button
+              className="link-button"
+              disabled={pageNum === maxPageNumber}
+              onClick={this.changePageNumber(maxPageNumber)}
+            >
+              Last
+            </button>
+          </span>
+        </div>
+        <div className="releases-index-items">
+          {!initialLoad ? (
+            <LoadingSpinner />
+          ) : (
+            <>
+              {loading && (
+                <div>
+                  <LoadingSpinner />
+                </div>
+              )}
+              <ul className="releases-index">
+                {Object.values(releases).map(release => (
+                  <ReleaseTile key={release._id} release={release} />
+                ))}
+              </ul>
+            </>
+          )}
+        </div>
       </div>
     );
   }
