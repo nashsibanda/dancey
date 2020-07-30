cc612679b5284933040bca9e5d5137daed7e9ac3
nashsibanda <contact@nashsibanda.com>
2020-07-30 13:30:32 +0900

    Add image to personnel select options

--- frontend/src/components/search/personnel_search_container.js
+++ frontend/src/components/search/personnel_search_container.js
@@ -11,26 +11,41 @@ import {
 } from "../../util/personnel_api_util";
 import SearchAutocomplete from "./search_autocomplete";
 import moment from "moment";
+import plainPersonnelImage from "../../assets/abstract-user-flat-1.svg";
 
 const formatPersonnelOptionLabel = ({
   value,
   label,
   moreInfoField1,
   moreInfoField2,
+  image,
 }) => (
   <div className="search-autocomplete-option personnel-option">
-    <div className="main-label">
-      <span>{label}</span>
+    <div className="image">
+      <img
+        src={image ? image.imageUrl : plainPersonnelImage}
+        className={image ? "main-image" : "main-image default-image"}
+        alt={
+          image
+            ? image.description
+            : "Default placeholder image - upload a new one!"
+        }
+      />
     </div>
-    <div className="more-info">
-      <span>
-        {moreInfoField1 && moreInfoField1.length > 0
-          ? `aka. ${moreInfoField1.join(", ")}`
-          : ""}
-      </span>
-      <span>
-        {moreInfoField2 ? `b. ${moment(moreInfoField2).format("YYYY")}` : ""}
-      </span>
+    <div className="label-and-details">
+      <div className="main-label">
+        <span>{label}</span>
+      </div>
+      <div className="more-info">
+        <span>
+          {moreInfoField1 && moreInfoField1.length > 0
+            ? `aka. ${moreInfoField1.slice(0, 4).join(", ")}`
+            : ""}
+        </span>
+        <span>
+          {moreInfoField2 ? `b. ${moment(moreInfoField2).format("YYYY")}` : ""}
+        </span>
+      </div>
     </div>
   </div>
 );
