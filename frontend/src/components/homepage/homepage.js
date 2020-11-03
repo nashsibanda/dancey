80e875f5c86a3a895d75890df4a77f4b89b0ae62
Nash Sibanda <contact@nashisbanda.com>
2020-11-03 21:27:46 +0000

    Add hero section component, nested under nav bar

---
+++ frontend/src/components/homepage/homepage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import HeroSection from "./hero_section";
+import HeroSectionContainer from "./hero_section_container";
+
+class HomePage extends React.Component {
+  render() {
+    return (
+      <div>
+        <HeroSectionContainer />
+      </div>
+    );
+  }
+}
+
+export default HomePage;
